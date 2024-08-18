03e2f5794c19a82b8a373b76019abcf73dc8e291
Kenost <Kenostru@mail.ru>
2024-08-18 18:43:28 +0500

    гзв

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -2,7 +2,16 @@ import axios from "axios";
 
 
 export async function getUser() {
-    const user = await axios.post(process.env.NEXT_PUBLIC_API + '/account')
+
+    var config = {
+        method: 'get',
+        maxBodyLength: Infinity,
+        url: process.env.NEXT_PUBLIC_API + '/account',
+        headers: { }
+    };
+      
+    const user = await axios(config)
+    // const user = await axios.get(process.env.NEXT_PUBLIC_API + '/account')
     console.log(user)
     return user
 }
